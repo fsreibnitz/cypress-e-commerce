57e2244f1d869a313ed3de6a286f6ab28e606a22
Fabio Reibnitz <fabiosr@bridge.ufsc.br>
2021-10-31 21:26:39 -0300

    e2e new user

---
+++ cypress/integration/authentication.spec.js
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+
+var faker = require('faker')
+
+describe('Authentication page', () => {
+	it('Creating a new account', () => {
+		cy.visit(Cypress.config('baseUrl'))
+		cy.get('.login').click()
+		cy.get('input[name=email_create]').type(faker.internet.email().toLowerCase())
+		cy.get('button[name=SubmitCreate]').click()
+		cy.get('div.account_creation h3.page-subheading').contains('personal').should('have.text','Your personal information')
+	});
+
+
+		it('Personal Information data', () => {
+		//	cy.visit(Cypress.config('baseUrl'))
+			cy.get('input#id_gender1').check()
+			cy.get('input[name=customer_firstname]').type(faker.name.firstName())
+			cy.get('input[name=customer_lastname]').type(faker.name.lastName())
+			cy.get('input[name=passwd').type(faker.internet.password())
+			cy.get('select#days').select('2')
+			cy.get('select#months').select('May')
+			cy.get('select#years').select('1990')
+
+		});
+
+			it('Your Adress data', () => {
+				cy.get('input#firstname').type(faker.name.firstName())
+				cy.get('input#lastname').type(faker.name.lastName())
+				cy.get('input#address1').type(faker.address.streetAddress())
+				cy.get('input#city').type(faker.address.city())
+				cy.get('select#id_country').select('United States')
+
+				cy.get('select#id_state').select('Georgia')
+				cy.get('input#postcode').type(faker.address.zipCode())
+				cy.get('input#phone_mobile').type(faker.phone.phoneNumber('0165#######'))
+				cy.get('button#submitAccount').click()
+			});
+
+			it('Account successfully created', () => {
+				cy.url().should('eq',  Cypress.config().baseUrl + 'index.php?controller=my-account')
+
+			
+		});
+
+});
