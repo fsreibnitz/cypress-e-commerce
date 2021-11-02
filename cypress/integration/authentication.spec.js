b404511a159119f29efeb3684b2a99d853690a73
Fabio Reibnitz <fabiosr@bridge.ufsc.br>
2021-11-02 07:59:03 -0300

    refactoring

--- cypress/integration/authentication.spec.js
+++ cypress/integration/authentication.spec.js
@@ -1,58 +1,44 @@
 /// <reference types="cypress" />
 
-var faker = require('faker')
+var faker = require("faker");
 
-describe('Authentication page', () => {
-	it('Creating a new account', () => {
-		cy.visit('/')
-		cy.get('.login').click()
-		cy.get('input[name=email_create]').type(faker.internet.email().toLowerCase())
-		cy.get('button[name=SubmitCreate]').click()
-		cy.get('div.account_creation h3.page-subheading').contains('personal').should('have.text','Your personal information')
-	});
+describe("Authentication page", () => {
+  it("Creating a new account", () => {
+    cy.visit("/");
+    cy.get(".login").click();
+    cy.get("input[name=email_create]").type(
+      faker.internet.email().toLowerCase()
+    );
 
+    cy.get("button[name=SubmitCreate]").click();
 
-		it('Personal Information data', () => {
-			cy.get('input#id_gender1').check()
-			cy.get('input[name=customer_firstname]').type(faker.name.firstName())
-			cy.get('input[name=customer_lastname]').type(faker.name.lastName())
-			cy.get('input[name=passwd').type(faker.internet.password())
-			cy.get('select#days').select('2')
-			cy.get('select#months').select('May')
-			cy.get('select#years').select('1990')
+    cy.url().should("include", "account-creation");
 
-		});
+    cy.log("**Personal Information data**");
+    cy.get("input#id_gender1").check();
+    cy.get("input[name=customer_firstname]").type(faker.name.firstName());
+    cy.get("input[name=customer_lastname]").type(faker.name.lastName());
+    cy.get("input[name=passwd").type(faker.internet.password());
+    cy.get("select#days").select("2");
+    cy.get("select#months").select("May");
+    cy.get("select#years").select("1990");
 
-			it('Your Adress data', () => {
-				cy.get('input#firstname').type(faker.name.firstName())
-				cy.get('input#lastname').type(faker.name.lastName())
-				cy.get('input#address1').type(faker.address.streetAddress())
-				cy.get('input#city').type(faker.address.city())
-				cy.get('select#id_country').select('United States')
+    cy.log("**Your Adress data**");
+    cy.get("input#firstname").type(faker.name.firstName());
+    cy.get("input#lastname").type(faker.name.lastName());
+    cy.get("input#address1").type(faker.address.streetAddress());
+    cy.get("input#city").type(faker.address.city());
+    cy.get("select#id_country").select("United States");
+    cy.get("select#id_state").select("Georgia");
+    cy.get("input#postcode").type(faker.address.zipCode("#####"));
+    cy.get("input#phone_mobile").type(faker.phone.phoneNumber("0165#######"));
+    cy.get("button#submitAccount").click();
 
-				cy.get('select#id_state').select('Georgia')
-				cy.get('input#postcode').type(faker.address.zipCode('#####'))
-				cy.get('input#phone_mobile').type(faker.phone.phoneNumber('0165#######'))
-			});
+    cy.log("**Account successfully created**");
 
-		
-			it('Account successfully created', () => {
-				cy.intercept('GET','**index.php?controller=my-account'
-				).as("myAccount");
-
-				cy.get('button#submitAccount').click()
-
-			
-
-				cy.wait("@myAccount")
-				cy.get("@myAccount").then((resMyaccont) => {
-					console.log(resMyaccont)
-					expect(resMyaccont.response.statusCode).to.eq(200);
-				});
-
-				cy.url().should('eq',  Cypress.config().baseUrl + 'index.php?controller=my-account')
-
-			
-		});
-
+    cy.url().should(
+      "eq",
+      Cypress.config().baseUrl + "index.php?controller=my-account"
+    );
+  });
 });
